0b00ada467da0b79db35616de4a2e315d537c081
kiddmary <marykidd@nypl.org>
2024-10-09 16:21:32 -0400

    Finished a lesson

--- AdvancedPython/session_01_working_files/inclass_exercises/inclass_1.10.py
+++ AdvancedPython/session_01_working_files/inclass_exercises/inclass_1.10.py
@@ -2,6 +2,8 @@
 #        and write only the US rows to the file.  Also write the header as the first row.
 #        Close the file, then look for newfile3.csv in the session directory.
 
+import csv
+
 rows = [
          [ 'company', 'state/province', 'country' ],
          [ 'Acme', 'Caliornia', 'US' ],
@@ -10,10 +12,23 @@ rows = [
          [ 'Beta', 'New York', 'US' ]
        ]
 
+us_rows = []
+
 fname = '../newfile3.csv'
 
 wfh = open(fname, 'w', newline='')
 writer = csv.writer(wfh)
+
+for row in rows:
+    if row[2] == 'US':
+        us_rows.append(row)
+
+writer.writerows(us_rows)
+wfh.close()
+
+
+
+
 
 
 
