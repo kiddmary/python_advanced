20fa25f6e22c33545cf03523833d26046764541e
kiddmary <marykidd@nypl.org>
2024-10-09 16:32:17 -0400

    Finished an exercise

--- AdvancedPython/session_01_working_files/inclass_exercises/inclass_1.11.py
+++ AdvancedPython/session_01_working_files/inclass_exercises/inclass_1.11.py
@@ -2,6 +2,8 @@
 # write only the company and country columns to the file.  Include the header.  Close the file, then
 # look for newfile4.csv in the session directory.
 
+import csv
+
 rows = [
          [ 'company', 'state/province', 'country' ],
          [ 'Acme', 'Caliornia', 'US' ],
@@ -10,9 +12,20 @@ rows = [
          [ 'Beta', 'New York', 'US' ]
        ]
 
+select_columns = []
+
 fname = '../newfile4.csv'
 
 wfh = open(fname, 'w', newline='')
 writer = csv.writer(wfh)
+
+for row in rows:
+    newrow = row[0:-1]
+    select_columns.append(newrow)
+
+print(select_columns)
+
+writer.writerows(select_columns)
+wfh.close()
 
 
