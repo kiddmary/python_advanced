53e22b17b67fce057dc57fb133a90766320bfae8
stresscarrot <mlk9157@nyu.edu>
2024-10-09 21:15:30 -0400

    More homework breakthroughs

--- AdvancedPython/session_01_working_files/select_columns.py
+++ AdvancedPython/session_01_working_files/select_columns.py
@@ -16,15 +16,19 @@ file_trg_name = sys.argv[4]
 fh = open(file_src_name, 'r')
 reader = csv.reader(fh)
 headers = next(reader)
-print(headers)
-
-for row in reader:
-    print(row)
-
-fh.close()
 
 wfh = open(file_trg_name, 'w', newline='')
 writer = csv.writer(wfh)
-writer.writerow([headers[0]])
+
+writer.writerow([headers[0], headers[1], headers[17], headers[19]])
+
+for row in reader:
+    sel_fields = [ row[0], row[1], row[17], row[19]]
+    writer.writerow(sel_fields)
+
+fh.close()
+wfh.close()
+
+
 
 
