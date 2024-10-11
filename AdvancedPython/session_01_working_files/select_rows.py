3aebc0e3294c6aea00eedd34ff686cfa9d311dea
stresscarrot <mlk9157@nyu.edu>
2024-10-11 10:59:36 -0400

    Finished second homework assignment

--- AdvancedPython/session_01_working_files/select_rows.py
+++ AdvancedPython/session_01_working_files/select_rows.py
@@ -1,5 +1,5 @@
 """
- select_columns.py -- write specific columns from a source to a target csv
+ select_rows.py -- write specific rows containing a specific statement acronym value
 
  Author: Mary Kidd (marykidd@nypl.org)
  Last Revised: 10/11/2024
@@ -14,11 +14,15 @@ parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]))
 
 parser.add_argument('-s', '--source', required=True)
 parser.add_argument('-t', '--target', required=True)
+parser.add_argument('-c', '--column', required=True)
+parser.add_argument('-v', '--value', required=True)
 
 args = parser.parse_args()
 
 file_src_name = sys.argv[2]
 file_trg_name = sys.argv[4]
+column_name = sys.argv[6]
+column_val = sys.argv[8]
 
 fh = open(file_src_name, 'r')
 reader = csv.reader(fh)
@@ -27,11 +31,9 @@ headers = next(reader)
 wfh = open(file_trg_name, 'w', newline='')
 writer = csv.writer(wfh)
 
-writer.writerow([headers[0], headers[1], headers[17], headers[19]])
+writer.writerow([headers[0], headers[1], headers[2]])
 
 for row in reader:
-    sel_fields = [ row[0], row[1], row[17], row[19]]
-    writer.writerow(sel_fields)
+    if row[1] == 'NY':
+        writer.writerow(row)
 
-fh.close()
-wfh.close()
